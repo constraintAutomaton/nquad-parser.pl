e0309df2c4e31d1d5cc8a45deaebb75304e09e3a
constraintAutomaton <Vous@exemple.com>
2025-04-05 13:47:48 +0200

    when clause for executing rule when variable is instantiated remplace with a freeze clause.

--- util.pl
+++ util.pl
@@ -1,5 +1,6 @@
 :- use_module(library(between)).
 :- use_module(library(dcgs)).
+:- use_module(library(freeze)).
 
 optional(_) --> "".
 optional(X) --> X.
@@ -11,12 +12,4 @@ zero_or_more(_) --> [].
 zero_or_more(X) --> X, zero_or_more(X).
 
 unicode_char_between(Char, MinDec, MaxDec) :-
-    when(
-            (   
-                nonvar(Char)
-            ),
-            (      
-                between(MinDec, MaxDec, X),
-                char_code(Char, X)
-            )
-    ).
+    freeze(Char, (between(MinDec, MaxDec, X), char_code(Char, X))).
