382cdc2479148d8349e6bcabd6fe60e24bc7a33c
constraintAutomaton <Vous@exemple.com>
2025-04-05 11:59:28 +0200

    zero or more predicate created

--- util.pl
+++ util.pl
@@ -7,6 +7,9 @@ optional(X) --> X.
 one_or_more(X) --> X.
 one_or_more(X) --> X, one_or_more(X).
 
+zero_or_more(_) --> [].
+zero_or_more(X) --> X, zero_or_more(X).
+
 unicode_char_between(Char, MinDec, MaxDec) :-
     when(
             (   
